a058cf28980dfbee6a3d9ff4f7200e8c03efe498
Phil Hendricks <pfhendri1@gmail.com>
2020-07-03 16:25:01 -0500

    refectored directory and added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+
 
 import Homepage from './pages/home/home.component'
+import './App.css';
+
+const AboutUs = () => (
+  <div>About US</div>
+) 
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path='/' exact component={Homepage} />
+        <Route path='/about-us' component={AboutUs} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image" style={{
       backgroundImage: `url(${imageUrl})`
-    }}/>
+    }} />
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
@@ -14,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
