4a84d608b374d53081965e3a289ff298603510db
Phil Hendricks <pfhendri1@gmail.com>
2020-07-07 20:41:57 -0500

    fixed small class error

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ const Header = () => (
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
-    <div class='options'>
+    <div className='options'>
       <Link className='option' to='/shop'>SHOP</Link>
       <Link className='option' to='/shop'>CONTACT</Link>
       <Link className='option' to='/signin'>SIGN IN</Link>
