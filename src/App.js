9c064fa58b3106382868b88e1b04ad2feb8a27bb
Phil Hendricks <pfhendri1@gmail.com>
2020-07-19 16:33:22 -0500

    Updated components to use redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import Header from './components/header/header.component'
 
 import HomePage from './pages/home/home.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
+
 
 import './App.css'
 
-function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-
+function App(props) {
   useEffect(() => {
+    const { setCurrentUser } = props
     const unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
@@ -37,7 +38,7 @@ function App() {
 
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header />
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -47,4 +48,8 @@ function App() {
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+})
+
+export default connect(null, mapDispatchToProps)(App);
