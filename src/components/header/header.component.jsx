0c315f549d9bfc7be1f5594e87e5398c36d629cc
Phil Hendricks <pfhendri1@gmail.com>
2020-07-19 16:52:47 -0500

    added cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -20,6 +21,7 @@ const Header = ({ currentUser }) => (
       { currentUser ? 
           <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
           <Link className='option' to='/signin'>SIGN IN</Link> }
+      <CartIcon />
     </div>
   </div>
 )
